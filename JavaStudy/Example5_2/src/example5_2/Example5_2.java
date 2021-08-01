804cac679fa59a4011f0ccb2feedf429327a7501
genki-iwai <76561536+genki-iwai@users.noreply.github.com>
2021-08-01 11:15:47 +0900

    4-4

--- JavaStudy/Example5_2/src/example5_2/Example5_2.java
+++ JavaStudy/Example5_2/src/example5_2/Example5_2.java
@@ -6,42 +6,46 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Example5_2 {
-	private static final String POSTGRE_DRIVER = "org.postgresql.Driver";
-	private static final String JDBC_CONNECTION = "jdbc:postgresql://locaohost:5432/lesson_bd";
-	private static final String USER = "postgres";
-	private static final String PASS = "postgres";
 
-	public static void main(String[] args) {
-		Connection connection = null;
-		Statement statement = null;
+    /* 定数 */
+    /** ドライバーのクラス名 */
+    private static final String POSTGRES_DRIVER = "org.postgresql.Driver";
+    /** ・JDMC接続先情報 */
+    private static final String JDBC_CONNECTION = "jdbc:postgresql://localhost:5432/lesson_db";
+    /** ・ユーザー名 */
+    private static final String USER = "postgres";
+    /** ・パスワード */
+    private static final String PASS = "postgres";
 
-		try{
-			Class.forName(POSTGRE_DRIVER);
-			connection = DriverManager.getConnection(JDBC_CONNECTION,USER,PASS);
-			statement = connection.createStatement();
+    public static void main(String[] args) {
 
-			String SQL = "INSERT INTO TB_SHOHIN( SHOHIN_ID, SHOHIN_NAME, TANKA)"
-					+ " VALUES('021', 'SHOHIN021', '2100')";
-			statement.executeUpdate(SQL);
+        Connection connection = null;
+        Statement statement = null;
 
-		}catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try{
+        try {
+            Class.forName(POSTGRES_DRIVER);
+            connection = DriverManager.getConnection(JDBC_CONNECTION,USER,PASS);
+            statement = connection.createStatement();
 
-				if (statement != null) {
-					statement.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			}catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+            String SQL = "INSERT INTO TB_SHOHIN( SHOHIN_ID, SHOHIN_NAME, TANKA)"
+            + " VALUES('021', 'SHOHIN021', 2100) ";
+            statement.executeUpdate(SQL);
 
-	}
-
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
